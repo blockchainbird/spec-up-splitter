82bafde8d47f55d81553bc65f457d21758d38de1
kor <kor@dwarshuis.com>
2024-06-10 15:05:18 +0200

    First commit

---
+++ help.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+/**
+ * @file This file:
+ * This file 
+    - splits terms and definitions into separate files based on the term.
+    ...
+ */
+
+console.log(`
+  This is a helpfile for the splitter script.
+
+  This is line two.
+  
+  This is line three.  
+  `);
